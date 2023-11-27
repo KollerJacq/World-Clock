ccb116cff2fedb730629b1ddeb58323cc02ded8e
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-11-27 13:44:15 +0100

    Added the actual time for each listed city

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 function updateTime() {
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-  newYorkTimeElement.innerHTML = `${newYorkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = `${newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   let shanghaiElement = document.querySelector("#shanghai");
-  let shanghaiDateElement = shanghaiElement.querySelector(".date");
-  let shanghaiTimeElement = shanghaiElement.querySelector(".time");
-  let shanghaiTime = moment().tz("Asia/Shanghai");
+  if (shanghaiElement) {
+    let shanghaiDateElement = shanghaiElement.querySelector(".date");
+    let shanghaiTimeElement = shanghaiElement.querySelector(".time");
+    let shanghaiTime = moment().tz("Asia/Shanghai");
 
-  shanghaiDateElement.innerHTML = shanghaiTime.format("MMMM Do YYYY");
-  shanghaiTimeElement.innerHTML = `${shanghaiTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    shanghaiDateElement.innerHTML = shanghaiTime.format("MMMM Do YYYY");
+    shanghaiTimeElement.innerHTML = `${shanghaiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   let istanbulElement = document.querySelector("#istanbul");
-  let istanbulDateElement = istanbulElement.querySelector(".date");
-  let istanbulTimeElement = istanbulElement.querySelector(".time");
-  let istanbulTime = moment().tz("Europe/Istanbul");
+  if (istanbulElement) {
+    let istanbulDateElement = istanbulElement.querySelector(".date");
+    let istanbulTimeElement = istanbulElement.querySelector(".time");
+    let istanbulTime = moment().tz("Europe/Istanbul");
 
-  istanbulDateElement.innerHTML = istanbulTime.format("MMMM Do YYYY");
-  istanbulTimeElement.innerHTML = `${istanbulTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    istanbulDateElement.innerHTML = istanbulTime.format("MMMM Do YYYY");
+    istanbulTimeElement.innerHTML = `${istanbulTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
 updateTime();
 setInterval(updateTime, 1);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  console.log(cityTime.format("MMMM Do YYYY"));
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        )}</div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
